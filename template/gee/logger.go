868c746afc52544a69024486b31ae23bb6b0c7dd
va1ner <yejiaqing08@gmail.com>
2020-11-11 19:42:54 +0800

    add gee_test.go in group and middleware; add day 6

---
+++ template/gee/logger.go
@@ -0,0 +1,15 @@
+package gee
+
+import (
+	"log"
+	"time"
+)
+
+// Logger record time
+func Logger() HandlerFunc {
+	return func(c *Context) {
+		t := time.Now()
+		c.Next()
+		log.Printf("[%d] %s in %v", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
